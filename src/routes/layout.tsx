1b9e01ab9fcd1253e0da40ae8197833eea672c08
YURII D <gkhelloworld@gmail.com>
2024-03-05 01:04:14 +0200

    Update layout.tsx

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,7 +1,17 @@
 import { component$, Slot } from "@builder.io/qwik";
+import type { RequestHandler } from "@builder.io/qwik-city";
 
 import Footer from "~/components/widgets/Footer";
 import Header from "~/components/widgets/Header";
+
+export const onGet: RequestHandler = async ({ cacheControl }) => {
+  cacheControl({
+    public: true,
+    staleWhileRevalidate: 600,
+    maxAge: 15,
+    sMaxAge: 30,
+  });
+};
 
 export default component$(() => {
   return (
